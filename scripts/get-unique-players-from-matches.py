8b721b7850c17f335779e3786d64104c55ba172e
mladen-995 <mladen.simijonovic@gmail.com>
2021-02-13 19:24:38 +0100

    Finalized filtering of players

--- scripts/get-unique-players-from-matches.py
+++ scripts/get-unique-players-from-matches.py
@@ -9,32 +9,27 @@ def createCsvOfUniquePlayerIds(inputCsv, outCsv):
     winnersAndLosers = np.concatenate((uniqueWinners, uniqueLosers))
     uniqueIds = np.unique(winnersAndLosers)
 
-    return uniqueIds
-
     df = pd.DataFrame({'player_id': uniqueIds[:]})
     df.to_csv(outCsv)
 
-playersFor2018 = createCsvOfUniquePlayerIds('./data/atp_matches_2018.csv', './data/C_players_2018.csv')
+    return uniqueIds
 
-playersFor2019 = createCsvOfUniquePlayerIds('./data/atp_matches_2019.csv', './data/C_players_2019.csv')
+playersFor2018 = createCsvOfUniquePlayerIds('./data/atp_matches_2018.csv', './data/HC_players_2018.csv')
 
-playersFor2020 = createCsvOfUniquePlayerIds('./data/atp_matches_2020.csv', './data/C_players_2020.csv')
+playersFor2019 = createCsvOfUniquePlayerIds('./data/atp_matches_2019.csv', './data/HC_players_2019.csv')
+
+playersFor2020 = createCsvOfUniquePlayerIds('./data/atp_matches_2020.csv', './data/HC_players_2020.csv')
 
 allPlayers = np.concatenate((playersFor2018, playersFor2019, playersFor2020))
 uniqueAllPlayers = np.unique(allPlayers)
 
 
 df = pd.DataFrame({'player_id': uniqueAllPlayers[:]})
-# df.to_csv('./data/HC_unique_players.csv')
-
-# print('There are ', df.size, ' unique players')
-
-
 
 allPlayersDf = pd.read_csv('./data/atp_players.csv', header=None)
-allPlayersDf = allPlayersDf[allPlayersDf[0].isin()] 
-allPlayersDf = allPlayersDf.drop(allPlayersDf[~allPlayersDf[0].isin(values=uniqueAllPlayers.tolist())].index)
+allPlayersDfFinal = allPlayersDf.drop(allPlayersDf[~allPlayersDf[0].isin(uniqueAllPlayers)].index)
 
-# allPlayersDf = allPlayersDf.apply(lambda x: np.nan if !isinstance(x[0], uniqueAllPlayers) else x).dropna(subset=[0]);
+allPlayersDfFinal.to_csv('./data/HC_players_all.csv')
 
-print(allPlayersDf.head())
+
+print('Number of filtered players ', len(allPlayersDfFinal.index))
