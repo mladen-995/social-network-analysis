6234cc74f796bf746fd2465680937cdb1f68b423
Sergije <sergijerazic@gmail.com>
2021-02-13 20:53:55 +0100

    #Fix for get-unique-players-from-matches script

--- scripts/get-unique-players-from-matches.py
+++ scripts/get-unique-players-from-matches.py
@@ -2,34 +2,38 @@ import pandas as pd
 import numpy as np
 
 
-def createCsvOfUniquePlayerIds(inputCsv, outCsv):
+def createCsvOfUniquePlayerIds(inputCsv):
     dataset = pd.read_csv(inputCsv)
     uniqueWinners = dataset.winner_id.unique()
     uniqueLosers = dataset.loser_id.unique()
     winnersAndLosers = np.concatenate((uniqueWinners, uniqueLosers))
     uniqueIds = np.unique(winnersAndLosers)
 
-    df = pd.DataFrame({'player_id': uniqueIds[:]})
-    df.to_csv(outCsv)
+    #df = pd.DataFrame({'player_id': uniqueIds[:]})
+    #df.to_csv(outCsv)
 
     return uniqueIds
 
-playersFor2018 = createCsvOfUniquePlayerIds('./data/atp_matches_2018.csv', './data/HC_players_2018.csv')
 
-playersFor2019 = createCsvOfUniquePlayerIds('./data/atp_matches_2019.csv', './data/HC_players_2019.csv')
+def filterPlayersByPlayerList(outputCsv, playerList):
+    playersDf = pd.read_csv('./data/atp_players.csv', header=None)
+    playersDfFinal = playersDf.drop(playersDf[~playersDf[0].isin(playerList)].index)
+    playersDfFinal.to_csv(outputCsv)
 
-playersFor2020 = createCsvOfUniquePlayerIds('./data/atp_matches_2020.csv', './data/HC_players_2020.csv')
+playersFor2018 = createCsvOfUniquePlayerIds('./data/atp_matches_2018.csv')
+
+playersFor2019 = createCsvOfUniquePlayerIds('./data/atp_matches_2019.csv')
+
+playersFor2020 = createCsvOfUniquePlayerIds('./data/atp_matches_2020.csv')
 
 allPlayers = np.concatenate((playersFor2018, playersFor2019, playersFor2020))
 uniqueAllPlayers = np.unique(allPlayers)
 
 
-df = pd.DataFrame({'player_id': uniqueAllPlayers[:]})
-
-allPlayersDf = pd.read_csv('./data/atp_players.csv', header=None)
-allPlayersDfFinal = allPlayersDf.drop(allPlayersDf[~allPlayersDf[0].isin(uniqueAllPlayers)].index)
-
-allPlayersDfFinal.to_csv('./data/HC_players_all.csv')
+filterPlayersByPlayerList('./data/HC_players_2018.csv', playersFor2018)
+filterPlayersByPlayerList('./data/HC_players_2019.csv', playersFor2019)
+filterPlayersByPlayerList('./data/HC_players_2020.csv', playersFor2020)
+filterPlayersByPlayerList('./data/HC_players_all.csv', uniqueAllPlayers)
 
 
-print('Number of filtered players ', len(allPlayersDfFinal.index))
+#print('Number of filtered players ', len(allPlayersDfFinal.index))
